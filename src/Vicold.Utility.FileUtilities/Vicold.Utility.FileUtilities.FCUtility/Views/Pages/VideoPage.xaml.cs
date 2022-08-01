0deed272a97daacee28718c61028d6c325fefaaf
Yin Yue <yiny@yusur.tech>
2022-08-01 17:43:44 +0800

    video

---
+++ src/Vicold.Utility.FileUtilities/Vicold.Utility.FileUtilities.FCUtility/Views/Pages/VideoPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LibVLCSharp.Shared;
+
+namespace Vicold.Utility.FileUtilities.FCUtility.Views.Pages
+{
+    /// <summary>
+    /// VideoPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class VideoPage : Page
+    {
+        private readonly LibVLC _libVLC;
+        private readonly MediaPlayer _mp;
+        
+        public VideoPage()
+        {
+            InitializeComponent();
+            _libVLC = new LibVLC();
+            _mp = new MediaPlayer(_libVLC);
+            VideoView.MediaPlayer = _mp;
+        }
+
+        public void Open()
+        {
+        }
+
+        private void DummyGrid_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void DummyGrid_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+        }
+    }
+}
